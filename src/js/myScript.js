70a2185fac59b466c2b5d796a11846363ce08633
ArtBelows <88453644+ArtBelows@users.noreply.github.com>
2021-09-05 14:27:40 +0300

    Добавил анимации и вычисления с помощью jquery

---
+++ src/js/myScript.js
@@ -0,0 +1,108 @@
+var valPrice1 = 0, valPrice2 = 0, valPrice3 = 0, valTerm1 = 0, valTerm2 = 0, valTerm3 = 0;
+
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        var scrollDistance = $(window).scrollTop();
+        $(".section").each(function(i, el)
+        {
+            i -= 1;
+            if($(el).offset().top - $("nav").outerHeight() <= scrollDistance)
+            {
+                $("nav a").each(function(i, el)
+                {
+                    if($(el).hasClass("active"))
+                    {
+                        $(el).removeClass("active");
+                    }
+                });
+                $("nav li:eq(" + i + ")").find("a").addClass("active");
+            }
+        });
+    });
+    
+    $('a[href^="#"]').click(function()
+    {
+        var valHref = $(this).attr("href");
+        $('html, body').animate({scrollTop: $(valHeight).offset.top});
+    });
+    
+    $("#select1 option").click(function()
+    {
+        valPrice1 = Number($(this).attr("money"));
+        valTerm1 = Number($(this).attr("term"));
+    });
+
+    $("#select2 option").click(function()
+    {
+        valPrice2 = Number($(this).attr("money"));
+        valTerm2 = Number($(this).attr("term"));
+    });
+
+    $("#select3 option").click(function()
+    {
+        valPrice3 = Number($(this).attr("money"));
+        valTerm3 = Number($(this).attr("term"));
+    });
+    
+    $("option").click(function()
+    {
+        var Price = valPrice1 + valPrice2 + valPrice3;
+        var Term = valTerm1 + valTerm2 + valTerm3;
+    
+        $("p.money").text(Price + "$");
+        $("p.term").text(Term + " дней");
+    });
+    
+    setTimeout(function()
+    {
+        $("#staticBackdropButton").trigger("click");
+    }, 30000);
+    
+    $(".image-link").magnificPopup({type:"image"});
+    
+    $('.roling').each(function()
+    {
+        $(this).prop('Counter',0).animate(
+        {Counter: $(this).text()},
+        {
+            duration: 2500,
+            easing: 'swing',
+            step: function (now)
+            {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    
+    $(".dot1").click(function()
+    {
+        $(this).addClass("selected_dot");
+        $(".dot2").removeClass("selected_dot");
+        $(".dot3").removeClass("selected_dot");
+        $(".report1").addClass("rep_active");
+        $(".report2").removeClass("rep_active");
+        $(".report3").removeClass("rep_active");
+    });
+    
+    $(".dot2").click(function()
+    {
+        $(".dot1").removeClass("selected_dot");
+        $(this).addClass("selected_dot");
+        $(".dot3").removeClass("selected_dot");
+        $(".report1").removeClass("rep_active");
+        $(".report2").addClass("rep_active");
+        $(".report3").removeClass("rep_active");
+    });
+    
+    $(".dot3").click(function()
+    {
+        $(".dot1").removeClass("selected_dot");
+        $(".dot2").removeClass("selected_dot");
+        $(this).addClass("selected_dot");
+        $(".report1").removeClass("rep_active");
+        $(".report2").removeClass("rep_active");
+        $(".report3").addClass("rep_active");
+    });
+});
